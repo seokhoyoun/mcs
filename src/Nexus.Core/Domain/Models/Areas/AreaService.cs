9f60fc52dd3c5eaf7dbaf02454c6d8faef0ed082
stone.youn <shyoun@j-onesoft.com>
2025-08-12 18:12:08 +0900

    loki

---
+++ src/Nexus.Core/Domain/Models/Areas/AreaService.cs
@@ -0,0 +1,25 @@
+﻿using Nexus.Core.Domain.Models.Locations;
+using Nexus.Core.Domain.Models.Locations.Enums;
+using Nexus.Core.Domain.Models.Transports.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.Core.Domain.Models.Areas
+{
+    internal class AreaService
+    {
+        private readonly LocationService _locationService;
+        private readonly List<Area> _areas = new();
+
+        public IReadOnlyList<Area> Areas => _areas;
+
+        public AreaService(LocationService locationService)
+        {
+            _locationService = locationService;
+
+        }
+    }
+}
