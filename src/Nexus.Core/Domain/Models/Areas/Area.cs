4e0ce7a12f7ec721060029f28635d03dde8dbed5
stone2on <devseokho@gmail.com>
2025-08-13 20:52:53 +0900

    area service 작성중

--- src/Nexus.Core/Domain/Models/Areas/Area.cs
+++ src/Nexus.Core/Domain/Models/Areas/Area.cs
@@ -11,8 +11,8 @@ namespace Nexus.Core.Domain.Models.Areas
 {
     public class Area : IEntity
     {
-        public required string Id { get; set; }
-        public required string Name { get; set; }
+        public string Id { get; private set; }
+        public string Name { get; private set; }
 
         public IReadOnlyList<Location<Cassette>> CassettePorts => _cassettePorts.AsReadOnly();
         public IReadOnlyList<Location<Tray>> TrayPorts => _trayPorts.AsReadOnly();
@@ -22,10 +22,11 @@ namespace Nexus.Core.Domain.Models.Areas
         private List<Location<Tray>> _trayPorts = new List<Location<Tray>>();
         private List<Set> _sets = new List<Set>();
 
-        public Area(string id, string name, List<Location<Cassette>> cassettePorts, List<Location<Tray>> trayPorts, List<Set> sets)
+        public Area(string id, string name, IEnumerable<Location<Cassette>> cassettePorts, IEnumerable<Location<Tray>> trayPorts, IEnumerable<Set> sets)
         {
             Id = id;
             Name = name;
+
             if (cassettePorts != null) _cassettePorts.AddRange(cassettePorts);
             if (trayPorts != null) _trayPorts.AddRange(trayPorts);
             if (sets != null) _sets.AddRange(sets);
