93ffe7df328a3a30b14991a21e335d8c68b3eba4
stone2on <devseokho@gmail.com>
2025-08-15 12:45:38 +0900

    st service update

--- src/Nexus.Core/Domain/Models/Plans/PlanStep.cs
+++ src/Nexus.Core/Domain/Models/Plans/PlanStep.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nexus.Core.Domain.Shared.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Nexus.Core.Domain.Models.Plans
 {
-    public class PlanStep
+    public class PlanStep : IEntity
     {
     }
 }
--- src/Nexus.Core/Domain/Models/Stockers/StockerService.cs
+++ src/Nexus.Core/Domain/Models/Stockers/StockerService.cs
@@ -1,4 +1,5 @@
 ﻿using Nexus.Core.Domain.Models.Locations;
+using Nexus.Core.Domain.Models.Stockers.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,22 @@ namespace Nexus.Core.Domain.Models.Stockers
 {
     public class StockerService
     {
-        private readonly LocationService _locationService;
+        private readonly IStockerRepository _stockerRepository;
         private readonly List<Stocker> _stockers;
 
+        public IReadOnlyList<Stocker> Stockers => _stockers.AsReadOnly();
+
+        public StockerService(IStockerRepository stockerRepository)
+        {
+            _stockerRepository = stockerRepository;
+            _stockers = new List<Stocker>();
+        }
+
+        public async Task InitializeStockerService()
+        {
+            var stockers = await _stockerRepository.GetAllStockersAsync();
+            _stockers.AddRange(stockers);
+        }
     }
+}
 }
