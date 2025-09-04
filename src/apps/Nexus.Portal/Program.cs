ccf1e9924185009d68468949f383364dd348e883
stone2on <devseokho@gmail.com>
2025-09-04 23:20:22 +0900

    cassette

--- src/apps/Nexus.Portal/Program.cs
+++ src/apps/Nexus.Portal/Program.cs
@@ -2,8 +2,12 @@
 using MudBlazor.Services;
 using Nexus.Core.Domain.Models.Areas.Interfaces;
 using Nexus.Core.Domain.Models.Areas.Services;
+using Nexus.Core.Domain.Models.Locations.Interfaces;
+using Nexus.Core.Domain.Models.Locations.Services;
 using Nexus.Core.Domain.Models.Stockers.Interfaces;
 using Nexus.Core.Domain.Models.Stockers.Services;
+using Nexus.Core.Domain.Models.Transports.Interfaces;
+using Nexus.Core.Domain.Models.Transports.Services;
 using Nexus.Core.Domain.Shared.Bases;
 using Nexus.Infrastructure.Persistence.Redis;
 using Nexus.Portal.Components;
@@ -23,9 +27,13 @@ namespace Nexus.Portal
 
             builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("redis:6379,abortConnect=false"));
 
-            builder.Services.AddScoped<ITransportsRepository, RedisTransportsRepository>();
-            builder.Services.AddScoped<IStockerRepository, RedisStockerRepository>();
-            builder.Services.AddScoped<IAreaRepository, RedisAreaRepository>();
+            builder.Services.AddSingleton<ITransportsRepository, RedisTransportsRepository>();
+            builder.Services.AddSingleton<IStockerRepository, RedisStockerRepository>();
+            builder.Services.AddSingleton<IAreaRepository, RedisAreaRepository>();
+            builder.Services.AddSingleton<ILocationRepository, RedisLocationRepository>();
+
+            builder.Services.AddSingleton<ILocationService, LocationService>();
+            builder.Services.AddSingleton<ITransportService, TransportService>();
 
             builder.Services.AddMudServices();
 
